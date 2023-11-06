dfd8b1637fc4c84fe13988fcd75b510c3a6796a0
wacfeld <wacfeld wang>
2023-11-06 14:47:06 -0500

    bad

--- doom.c
+++ doom.c
@@ -3,7 +3,7 @@
 #include <unistd.h>
 #include <string.h>
 
-#define USAGESTR "USAGE: ./doom FILE\n"
+#define USAGESTR "USAGE: ./doom FILE"
 
 char template[] = "/tmp/doom.XXXXXX";
 
@@ -27,11 +27,21 @@ void terminate()
   }
 }
 
+char* concat(const char *s1, const char *s2)
+{
+    const size_t len1 = strlen(s1);
+    const size_t len2 = strlen(s2);
+    char *result = malloc(len1 + len2 + 1);
+    memcpy(result, s1, len1);
+    memcpy(result + len1, s2, len2 + 1);
+    return result;
+}
+
 int main(int argc, char **argv)
 {
   if(argc <= 1)
   {
-    printf(USAGESTR);
+    puts(USAGESTR);
     return 1;
   }
 
@@ -39,5 +49,11 @@ int main(int argc, char **argv)
   puts(fname);
   init();
   puts(template);
+
+  char *dest = concat(template, "/file.tex");
+
+  symlink(fname, dest);
+  getchar();
+
   terminate();
 }
